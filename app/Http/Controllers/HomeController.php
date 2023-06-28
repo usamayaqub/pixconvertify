6a6b5a5dd825eec2799b723a128e6008df8f9047
unknown <usamayaqub302@gmail.com>
2023-06-28 15:35:22 +0500

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,20 +1,43 @@
 <?php
 
 namespace App\Http\Controllers;
+use Intervention\Image\Facades\Image;
 
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function convertImage(Request $request)
+    {
+        // Get the uploaded image file
+        $imageFile = $request->file('image');
+        // Define the desired image format
+        $format = $request->input('format');
+    
+        // Compress and convert the image
+        $convertedImage = Image::make($imageFile)->encode($format, 75);
+    
+        // Generate a unique filename
+        $filename = uniqid().'.'.$format;
+    
+        // Save the converted image to the public disk
+        $convertedImagePath = public_path('converted/'.$filename);
+        $convertedImage->save($convertedImagePath);
+    
+        // Return the downloadable link for the converted image
+        return response()->json([
+            'url' => asset('converted/'.$filename),
+        ]);
+    }
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
