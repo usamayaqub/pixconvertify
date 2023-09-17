d00a536bf7eb58663ef9f334f2ac79e6486fa345
danish334 <danish@cynosuredesigns.co.uk>
2023-09-17 17:05:33 +0500

    bug fixed

--- resources/views/responsiveness.blade.php
+++ resources/views/responsiveness.blade.php
@@ -59,6 +59,68 @@
       </div>
   </div>
 
+    
+    <section class="section-convertion-and-format mb-0 mt-0">
+      <div class="container">
+        <h3 class="h1 mb-5 center">Check Your Website's Mobile Responsive Design with PixConvertify</h3>
+          <div class="flex-100 0 bg-f4">
+            <div class="flex-inner">              
+              <div>
+              <h3 class="heading-md">PixConvertify's Online Responsive Testing Tool</h3>
+                <p class="">
+                  Are you concerned about your website's mobile responsiveness? Ensuring your website adapts gracefully to different screens is crucial in today's digital landscape. PixConvertify is here to assist you in evaluating your website's quick responsiveness online. We understand the significance of a website being mobile-responsive, whether it's an HTML website or built with any other technology. 
+
+                  <br>
+                  <br>
+                  PixConvertify simplifies the process of checking responsive online. Our online responsiveness testing tool enables you to input your website's URL and view it simultaneously on four different screens
+                </p>
+                <br>
+                <ul class="responsiveness_ul">
+                  <h4>Desktop:</h4>
+                    <p>
+                      See how your site appears on a standard computer monitor.
+                    </p>
+                  <h4>Laptop:</h4>
+                    <p> 
+                      Evaluate its responsiveness on a typical laptop screen.
+                    </p>
+                  <h4>iPad:</h4>
+                    <p>
+                      Examine how your site performs on a tablet.
+                    </p>
+                    <h4>Mobile:</h4>
+                    <p>
+                      Ensure that your website is mobile-friendly and offers a great user experience.
+                    </p>
+                  </ul>
+              </div>
+            </div>
+          </div>
+        <div class="flex-wrap align-items-start">
+            <div class="flex-50 bg-f4">
+              <div class="flex-inner">              
+                <div>
+                  <h3 class="heading-md">Start Testing Your Website Now</h3>
+                    <p class="">
+                      Ready to assess your website's website mobile responsiveness and quick responsiveness online? Enter your URL below and click the "Check Now " button to explore your site's performance on different devices. PixConvertify empowers you to gain valuable insights into your website's adaptability and ensure it delivers a seamless experience to all your visitors. Don't leave the mobile-responsive aspect of your site to chance – test it online today and optimize your web presence.
+                    </p>
+                </div>
+              </div>
+          </div>
+          <div class="flex-50 bg-f4">
+              <div class="flex-inner">              
+                <div>
+                  <h3 class="heading-md">Why Test Responsive Online?</h3>
+                    <p class="">
+                      Testing the responsiveness of your website is vital for various reasons. It guarantees that your website is accessible and user-friendly across desktops, laptops, tablets, and mobile phones. With mobile device usage on the rise, having a website that's responsive is no longer just a nice-to-have – it's a necessity. Additionally, search engines prioritize mobile-friendly websites, improving your website's chances of ranking higher in search results. Plus, a responsive site enhances the overall user experience.
+                    </p>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
 
 
   <div id="iframe-error-modal" class="modal">
