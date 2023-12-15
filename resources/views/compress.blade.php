6513fb224484acd59845c256c58590ced41e828d
danish334 <danish@cynosuredesigns.co.uk>
2023-12-15 13:57:44 +0500

    compress content change

--- resources/views/compress.blade.php
+++ resources/views/compress.blade.php
@@ -3,7 +3,11 @@
 @section('meta_title', 'Image Compression | Compress PNG, WebP, JPEG, JPG, GIF | PixConvertify')
 @section('meta_description', 'Compress your images effortlessly at PixConvertify! Reduce file sizes of PNG, WebP, JPEG, JPG, and GIF images without compromising quality. Optimize your visual content for faster loading times and improved web performance. Try our user-friendly image compression tools and enjoy smaller, lighter, and faster images. Enhance your website or projects today!')
 @section('canonical',"https://pixconvertify.com/compress")
-
+<style>
+  .section-convertion-and-format .flex-wrap .content-p{
+    min-height: auto !important;
+  }
+</style>
 @section('script_css')
 <meta itemprop="image" content="">
 <meta property="og:image" content="" />
@@ -94,13 +98,9 @@
           <div class="flex-50">
             <div class="flex-inner">              
               <div>
-                  <h3 class="heading-md">Shrink Images Online for Easy Storage</h3>
+                  <h3 class="heading-md">JPEG/JPG</h3>
                   <p class="content-p">
-                    Do you find your image files taking up way too much space on your device? No worries! You can easily shrink images online using our convenient tool. Whether you're managing a website, sharing pictures on social media, or just trying to save storage, we've got you covered.
-                    
-                    With our user-friendly platform, you can quickly and effortlessly compress your images. Plus, you can process up to 40 images at once, making it a breeze to optimize your visual content. So, if you're tired of bulky image files eating up your storage, give our Shrink Images Online tool a try today. Streamline your images and free up space effortlessly!
-                    
-                    So, if you're tired of bulky image files eating up your storage, give our Shrink Images Online tool a try today. Streamline your images and free up space effortlessly!
+                    Easily convert your JPEG files into JPG files using this online converter. By using CloudConvert, you will have the ability to convert any of your picture documents on the web. We support a wide range of file types, including PNG, JPG, GIF, WEBP, and HEIC, among many other formats.
                   <p>
                 </div>
                 <img src="{{asset('./assets/images/shrink.svg')}}" alt="Shrink Images Online">
@@ -109,12 +109,9 @@
           <div class="flex-50">
             <div class="flex-inner">              
               <div>
-              <h3 class="heading-md">The Benefits of Online Picture Compression in Web Design</h3>
+              <h3 class="heading-md">PNG</h3>
                 <p class="content-p">
-                  In the ever-evolving field of web design, where speed and performance are key, online picture compression proves to be a valuable tool. It's not just about making your website look polished; it's about making your site work better.
-                  Online picture compression simplifies the process of reducing the file size of your images, which, in turn, ensures that your web pages load swiftly. This significantly improves the user experience, especially for those accessing your site on mobile devices.
-                  Moreover, online picture compression helps you save on bandwidth, which can be quite helpful if you're working within budget constraints or expecting heavy website traffic.
-                  In the world of web design, remember this simple truth Embrace online picture compression for a faster, more efficient, and budget-friendly website.
+                  PNG means "Versatile Illustration Configuration". Uncompressed raster pictures are most often used on the web. Designed to replace Illustrations Exchange Format (GIF). The PNG file format has no copyright restrictions. In addition to GIF images, PNGs show simple foundations.
                 <p>
               </div>
                 <img src="{{asset('./assets/images/layer-ui-web.svg')}}" alt="Online Picture Compression">
@@ -123,32 +120,20 @@
           <div class="flex-50">
             <div class="flex-inner">              
               <div>
-                  <h3 class="heading-md"><b>Optimizing Load Times</b> The Significance of Compressing Online Images</h3>
+                <h3 class="heading-md">WEBP</h3>
                   <p class="content-p">
-                    When it comes to speeding up your website, think about image compression as your secret weapon. By compressing images online, you're not just making them smaller; you're making your web pages load faster.
-
-                    When visitors come to your site, and because you decided to compress online images, your pages load quickly. No more frustrating waiting times; it's nearly instant.
-
-                    By using simple methods to compress online images, you're not only improving the user experience but also saving valuable bandwidth. Whether you have a portfolio or an online store, this approach ensures that your web content is easily accessible to more people without sacrificing quality.
-
-                    So, if you want to maximize page load speed, keep this in mind compress online images is the key to a faster, smoother online experience. Compress an image now and reduce size while preserving quality and resolution
+                    The WEBG file format is a raster graphics file format developed by Google, which is intended to replace the JPEG, PNG, and GIF formats for raster graphics files. Aside from supporting both lossy and lossless compression, it also supports animations and alpha transparent images.
                   <p>
               </div>
-                <img src="{{asset('./assets/images/setting.svg')}}" alt="compress online images">
-            </div>
+                  <img src="{{asset('./assets/images/speed.svg')}}" alt="Online Compress Size of Image">
+              </div>
           </div>
           <div class="flex-50">
             <div class="flex-inner">              
               <div>
-                <h3 class="heading-md">Simplifying Your Website Online Compress Size of Image Explained</h3>
+                <h3 class="heading-md">GIF</h3>
                   <p class="content-p">
-                    When it comes to boosting your website's performance, understanding the online compress size of image process is like having a friendly conversation. By applying online compress size of image techniques, you're ensuring your website loads swiftly and efficiently.
-
-                    Your website, optimized with online compress size of image methods, welcomes visitors with lightning-fast page loading. Waiting around is a thing of the past, thanks to efficient image compression.
-
-                    Incorporating the practice of online compress size of image is a wise choice. It not only enhances user experience but also conserves valuable bandwidth. Whether you're running a blog or an e-commerce site, it's a win-win situation – your website remains accessible and performs at its peak.
-
-                    So, keep in mind the key phrase online compress size of image to keep your website running efficiently and user-friendly.      
+                    GIF stands for Graphics Interchange Format. The GIF format is a format for storing bitmap images. Typically, GIF images are compressed using a lossless compression method called Lempel-Ziv-Welch (LZW) in order to reduce the file size without degrading the quality of the images.
                   <p>
               </div>
                   <img src="{{asset('./assets/images/speed.svg')}}" alt="Online Compress Size of Image">
@@ -163,19 +148,19 @@
             <h2>How does it work?</h2>
             <div class="how-its-work_wrap">
                 <div class="how-its-work_item">
-                    <img src="./assets/images/file.svg" alt="">
+                    <img class="icons-image" src="{{asset('./assets/images/file.svg')}}" alt="">
                     <h3>Step 1</h3>
-                    <p>Select a file from your devices.</p>
+                    <p>Simply upload your image files or drag and drop your image files directly onto our platform. </p>
                 </div>
                 <div class="how-its-work_item">
-                    <img src="./assets/images/format.svg" class="icons-image" alt="">
+                    <img src="{{asset('./assets/images/format.svg')}}" class="icons-image" alt="">
                     <h3>Step 2</h3>
-                    <p>Click on the Compress button.</p>
+                    <p>Choose the Desired Output Format for your converted images from our extensive list.</p>
                 </div>
                 <div class="how-its-work_item">
-                    <img src="./assets/images/download.svg" alt="">
+                    <img class="icons-image" src="{{asset('./assets/images/download.svg')}}" alt="">
                     <h3>Step 3</h3>
-                    <p>Download your compressed file immediately.</p>
+                    <p>Once the conversion is complete, download your converted images.</p>
                 </div>
             </div>
         </div>
